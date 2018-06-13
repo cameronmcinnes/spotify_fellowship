391325ea30e4d5a7a63a172efe4f4f7fa049f2fe
cameron mcinnes <cam.mcinnes@gmail.com>
2018-06-13 10:26:42 -0400

    Add change possibilities using tabulation

--- change_possibilities.js
+++ change_possibilities.js
@@ -1,5 +1,6 @@
-function changePossibilities(coins, target, currCoinIdx = coins.length - 1) {
+function changePossibilities(coins, target, currCoinIdx = coins.length - 1, memo = {}) {
   if (target === 0) {
+    memo[]
     return 1;
   } else if (target < 0 || currCoinIdx < 0) {
     return 0;
@@ -9,4 +10,18 @@ function changePossibilities(coins, target, currCoinIdx = coins.length - 1) {
 
   return (changePossibilities(coins, target, currCoinIdx - 1) +
     changePossibilities(coins, target - currCoin, currCoinIdx));
+}
+
+function changeTabulation(coins, target) {
+    const tab = new Array(target + 1).fill(0);
+
+    tab[0] = 1;
+
+    for(let i = 0; i < coins.length; i++) {
+      for (let j = coins[i]; j <= target; j++) {
+        tab[j] += tab[j-coins[i]];
+      }
+    }
+
+    return tab[target];
 }
