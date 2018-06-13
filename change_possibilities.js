23fb431a09ae809f874780bf6b54a2640a671202
cameron mcinnes <cam.mcinnes@gmail.com>
2018-06-13 11:02:11 -0400

    Memoize recursive change solution

--- change_possibilities.js
+++ change_possibilities.js
@@ -1,18 +1,8 @@
-function changePossibilities(coins, target, currCoinIdx = coins.length - 1, memo = {}) {
-  if (target === 0) {
-    memo[]
-    return 1;
-  } else if (target < 0 || currCoinIdx < 0) {
-    return 0;
-  }
+// Fastest Solution using tabulation
+// Time Complexity O(mn) where m is the number of coins and n is target
+// Space Complexity: O(n) where n is target
 
-  const currCoin = coins[currCoinIdx];
-
-  return (changePossibilities(coins, target, currCoinIdx - 1) +
-    changePossibilities(coins, target - currCoin, currCoinIdx));
-}
-
-function changeTabulation(coins, target) {
+function changePossibilities(coins, target) {
     const tab = new Array(target + 1).fill(0);
 
     tab[0] = 1;
@@ -24,4 +14,34 @@ function changeTabulation(coins, target) {
     }
 
     return tab[target];
+}
+
+// Slower solution using recursion and memoization
+// Time complexity: O(2^n) where n is the size of the target
+// Space complexity: O(2^n)
+
+function changePossibilities(coins, target) {
+  const memo = {};
+  return _changeStep(coins, target, coins.length - 1, memo);
+}
+
+function _changeStep(coins, target, currCoinIdx, memo) {
+  const memoKey = target + '|' + currCoinIdx;
+
+  if (target === 0) {
+    memo[memoKey] = 1;
+    return 1;
+  } else if (target < 0 || currCoinIdx < 0) {
+    memo[memoKey] = 0;
+    return 0;
+  }
+
+  if (memo[memoKey] !== undefined) {
+    return memo[memoKey];
+  }
+
+  const currCoin = coins[currCoinIdx];
+
+  return (_changeStep(coins, target, currCoinIdx - 1, memo) +
+  _changeStep(coins, target - currCoin, currCoinIdx, memo));
 }
